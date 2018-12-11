d03150ec2910f9f4c2757f6224d8f6ad10db72d6
Chris <chrismcrow@gmail.com>
2018-12-11 11:32:07 -0800

    Updates

---
+++ precache-manifest.80702a66c2997cb245e2a276068acc2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76fdc5049c2ed996a2c2c132dc0b409a",
+    "url": "/UltimateTicTacToe-react/static/media/Space-Desktop.76fdc504.jpg"
+  },
+  {
+    "revision": "80c35a0177985d8b906d",
+    "url": "/UltimateTicTacToe-react/static/js/runtime~main.80c35a01.js"
+  },
+  {
+    "revision": "546d07892e211d3db236",
+    "url": "/UltimateTicTacToe-react/static/js/main.546d0789.chunk.js"
+  },
+  {
+    "revision": "360916dfe3d720d287fb",
+    "url": "/UltimateTicTacToe-react/static/js/1.360916df.chunk.js"
+  },
+  {
+    "revision": "546d07892e211d3db236",
+    "url": "/UltimateTicTacToe-react/static/css/main.27a6d0e9.chunk.css"
+  },
+  {
+    "revision": "360916dfe3d720d287fb",
+    "url": "/UltimateTicTacToe-react/static/css/1.07728b99.chunk.css"
+  },
+  {
+    "revision": "6129d5c641a4f2987d3be71072335fd8",
+    "url": "/UltimateTicTacToe-react/index.html"
+  }
+];
